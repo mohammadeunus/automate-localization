7316368e86ecf92a82fbad8883ecfc49526c6c04
Mohammad Eunus <100eunus@gmail.com>
2024-05-20 18:51:28 +0600

    update

--- automatice-localizaton/Program.cs
+++ automatice-localizaton/Program.cs
@@ -4,18 +4,12 @@ class Program
 {
     static void Main()
     {
-        string filePath = Console.ReadLine();
+        string filePath = "F:\\sampleAbpProject\\RSLog2.0\\src\\Rocscience.RSLog.Web\\Pages";
         string jsonPath = "C:\\Users\\User\\source\\repos\\automatice-localizaton\\automate-localization\\automatice-localizaton\\en.json";
 
-        #region abstract
         Dictionary<string, string> stringKeys = Read_json_file.GetKeyValuePair(jsonPath);
 
-        List<string> strings = Read_cshtml_file.ExtractStringsFromCSHTML(filePath);
-        List_dictionary_modifier.GetMatchedKeyValues(strings, stringKeys);
-        #endregion
-
         Read_cshtml_file.OverrideFile(filePath, stringKeys);
-
     }
 
 }
